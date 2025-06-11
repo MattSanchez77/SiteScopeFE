48eb4566b6877fce72b58009abf6a444c296984b
MattSanchez77 <matthew.sanchez77@icloud.com>
2025-06-11 11:35:32 -0600

    Added CSS formating

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Nav/NavBar';
 import Home from './pages/HomePage';
+import About from './pages/About';
 
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
     <Navbar />
     <Routes>
     <Route path='/' element={<Home />} />
+    <Route path='/about' element={<About />} />
+
     </Routes>
     </>
   )
